96d5d2289a9268f94699ad907b7562d0e5bbc324
Aurora Hart <aurora.239576@nv.ccsd.net>
2020-09-04 15:40:04 +0000

    Finished Time Stamps

--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -15,9 +15,24 @@ class Timestamps {
         int seconds2 = scanner.nextInt();
 
 
-        /*
-         *  your code goes here
-         */
+        //changes the hours and minutes into seconds
+        int sechours1 = hours1 * 3600;
+        int secminutes1 = minutes1 * 60;
+
+        int sechours2 = hours2 * 3600;
+        int secminutes2 = minutes2 * 60;
+
+        //Subtracts the sceconds form hours 2 by hours 1
+        int deltahours = sechours2 - sechours1;
+        int deltaminutes = secminutes2 - secminutes1;
+        int deltasec = seconds2 - seconds1;
+
+        //adds the second together
+        int deta = deltahours + deltaminutes + deltasec;
+
+        //displays how many second it has been between the time stamps
+        System.out.print(deta);
+
 
 
         // closing the scanner object
